89bd29711f4ec86909cab24f5fd75764c4df2aa1
karatehcops <contact@lazers.tv>
2018-03-19 13:21:34 -0400

    Enhance logging.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import chalk from 'chalk';
 
 // Instantiate our express application.
 const app = express();
 
 // Easy logging.
-const log = json => console.log(chalk.green(JSON.stringify(json, null, 2)));
+const log = (output, color = chalk.green) => typeof output === 'object'
+  ? console.log(color(JSON.stringify(output, null, 2)))
+  : console.log(color(output));
 
 // Create a GET route to our base url.
 app.get('/', (req, res) => {
