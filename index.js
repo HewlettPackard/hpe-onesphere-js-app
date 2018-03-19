c77e5dc44910db78a945b5acfb0af183778a4027
karatehcops <contact@lazers.tv>
2018-03-19 19:09:52 -0400

    Add cors.

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const log = (output, color = chalk.green) => (typeof output === 'object'
   ? console.log(color(JSON.stringify(output, null, 2)))
   : console.log(color(output)));
 
+// Allow all origins.
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Create a GET route to our base url.
 app.get('/', (req, res) => {
   res.send('Hello World');
