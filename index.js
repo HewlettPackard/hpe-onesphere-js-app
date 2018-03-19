ba1c2b09b474ef8e0bec725f22aa523727dc177d
karatehcops <contact@lazers.tv>
2018-03-19 14:12:50 -0400

    Add airbnb lint config.

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import chalk from 'chalk';
 const app = express();
 
 // Easy logging.
-const log = (output, color = chalk.green) => typeof output === 'object'
+const log = (output, color = chalk.green) => (typeof output === 'object'
   ? console.log(color(JSON.stringify(output, null, 2)))
-  : console.log(color(output));
+  : console.log(color(output)));
 
 // Create a GET route to our base url.
 app.get('/', (req, res) => {
@@ -17,5 +17,5 @@ app.get('/', (req, res) => {
 // Listen for requests at a specified port, 3000.
 app.listen(3001, () => {
   // Send a message to our terminal window that we're ready for action.
-  console.log('Server listening to http://localhost:3001');
+  log('Server listening to http://localhost:3001');
 });
